6be5af84933ff282890e05622bee037a4f410196
unknown <https://github.com/Pooja-bhaskaran/Day03.git>
2022-07-08 18:45:36 +0530

    uc1 firstname is added

--- src/com/Bridgelabz/LambdaExpression.java
+++ src/com/Bridgelabz/LambdaExpression.java
@@ -11,6 +11,15 @@ interface UserRegistration {
 public class LambdaExpression{
     public static void main(String[] args) {
         System.out.println("User Registration program using LambdaExpression");
+        UserRegistration function =(name)->{
+            boolean check = Pattern.matches("([A-Z]{1}[a-z]{1,})", name);
+            if (check == true){
+                System.out.println("Firstname is valid");
+            }
+            else
+                System.out.println("Firstname is not valid ,Please enter valid name");
+        };
+        function.firstname("Pooja");
     }
 }
 
