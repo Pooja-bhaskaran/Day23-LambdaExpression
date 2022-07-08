38c43015ee062b822ddea0a2e3c966e1226b0a15
unknown <https://github.com/Pooja-bhaskaran/Day03.git>
2022-07-08 18:40:25 +0530

    Welcome to UserRegistration using Lambda

---
+++ src/com/Bridgelabz/LambdaExpression.java
@@ -0,0 +1,16 @@
+package com.Bridgelabz;
+
+import java.util.regex.Pattern;
+
+@FunctionalInterface
+interface UserRegistration {
+    void firstname(String name); // single abstract method
+
+}
+
+public class LambdaExpression{
+    public static void main(String[] args) {
+        System.out.println("User Registration program using LambdaExpression");
+    }
+}
+
